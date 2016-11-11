5ed5001023ea81b7fe09b4f3dc7eab103ccec75a
unknown <sebaseifert@gmail.com>
2016-11-11 12:00:42 -0300

    agregado de extensión de vistaa modulo base

--- document_control_base/__openerp__.py
+++ document_control_base/__openerp__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','expirations'],
+    'depends': ['base','expirations','ir_ui_view_extension'],
 
     # always loaded
     'data': [
